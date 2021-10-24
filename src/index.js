aee2a52a40bee2be190b3ef0aac3a70512d7398d
Jose Pereira <josepsneto@gmail.com>
2021-10-24 14:37:11 -0300

    OK

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ app.use(cors());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/accounts', accountsRouter);
 
-console.log('Just testing')
+console.log('Just testing rp')
 
 app.listen(3000, async () => {
 
