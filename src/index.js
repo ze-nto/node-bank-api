86178480fcdcbf56ed56d98627348b1f6886f8be
Jose Pereira <josepsneto@gmail.com>
2021-11-07 19:35:49 -0300

    [ NEW ] Implements the query and mutations schemas

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import swaggerUi from 'swagger-ui-express';
 import { swaggerDoc } from '../doc.js';
 import { buildSchema } from 'graphql';
 import { graphqlHTTP } from 'express-graphql';
+import AccountService from './services/account.service.js';
 
 const { readFile, writeFile } = fs;
 const { combine, timestamp, label, printf } = winston.format;
@@ -18,11 +19,37 @@ const schema = buildSchema(`
         name: String
         balance: Float
     }
+    input AccountInput{
+        id: Int
+        name: String
+        balance: Float
+    }
     type Query {
         getAccounts: [Account]
         getAccount(id: Int): Account
     }
+    type Mutation {
+        createAccount(account: AccountInput): Account
+        deleteAccount(id: Int): Boolean
+        updateAccount(Account: AccountInput): Account
+    }
 `);
+
+const root = {
+    getAccounts: () => AccountService.getAccounts(),
+    getAccount(args) {
+        return AccountService.getAccount(args.id)
+    },
+    createAccount({account}){
+        return AccountService.createAccount(account)
+    },
+    deleteAccount(args){
+        return AccountService.deleteAccount(args.id)
+    },
+    updateAccount({account}){
+        return AccountService.updateAccount(account)
+    }
+}
 
 const app = express();
 const myFormat = printf(({level, message, label, timestamp}) => {
@@ -51,7 +78,7 @@ app.use('/accounts', accountsRouter);
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
-    rootValue: null,
+    rootValue: root,
     graphiql: true 
 }));
 
